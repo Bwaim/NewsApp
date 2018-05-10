5017741810afb9c798e33ed468047f9e6af2302e
Fabien Boismoreau <fabien.boismoreau@gmail.com>
2018-05-10 10:57:49 +0200

    feat: Add the News loader class

---
+++ app/src/main/java/com/bwaim/newsapp/loaders/NewsLoader.java
@@ -0,0 +1,75 @@
+/*
+ *    Copyright 2018 Fabien Boismoreau
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.bwaim.newsapp.loaders;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import com.bwaim.newsapp.model.News;
+
+import java.util.List;
+
+/**
+ * Created by Fabien Boismoreau on 10/05/2018.
+ * <p>
+ */
+public class NewsLoader extends AsyncTaskLoader<List<News>> {
+
+    public NewsLoader(Context context) {
+        super(context);
+    }
+
+    /**
+     * Subclasses must implement this to take care of loading their data,
+     * as per {@link #startLoading()}.  This is not called by clients directly,
+     * but as a result of a call to {@link #startLoading()}.
+     */
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    /**
+     * Called on a worker thread to perform the actual load and to return
+     * the result of the load operation.
+     * <p>
+     * Implementations should not deliver the result directly, but should return them
+     * from this method, which will eventually end up calling {@link #deliverResult} on
+     * the UI thread.  If implementations need to process the results on the UI thread
+     * they may override {@link #deliverResult} and do so there.
+     * <p>
+     * To support cancellation, this method should periodically check the value of
+     * {@link #isLoadInBackgroundCanceled} and terminate when it returns true.
+     * Subclasses may also override {@link #cancelLoadInBackground} to interrupt the load
+     * directly instead of polling {@link #isLoadInBackgroundCanceled}.
+     * <p>
+     * When the load is canceled, this method may either return normally or throw
+     * {@link android.os.OperationCanceledException}.  In either case, the
+     * {@link android.content.Loader} will call {@link #onCanceled} to perform
+     * post-cancellation cleanup and to dispose of the result object, if any.
+     *
+     * @return The result of the load operation.
+     * @throws android.os.OperationCanceledException if the load is canceled during execution.
+     * @see #isLoadInBackgroundCanceled
+     * @see #cancelLoadInBackground
+     * @see #onCanceled
+     */
+    @Override
+    public List<News> loadInBackground() {
+        return null;
+    }
+}
