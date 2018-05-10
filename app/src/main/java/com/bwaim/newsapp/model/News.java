550573c376b8351727e26882aa02236322afeb81
Fabien Boismoreau <fabien.boismoreau@gmail.com>
2018-05-10 10:45:16 +0200

    feat: Add News class

---
+++ app/src/main/java/com/bwaim/newsapp/model/News.java
@@ -0,0 +1,68 @@
+/*
+ *    Copyright 2018 Fabien Boismoreau
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.bwaim.newsapp.model;
+
+/**
+ * Created by Fabien Boismoreau on 10/05/2018.
+ * <p>
+ */
+public class News {
+    private String title;
+
+    private String sectionName;
+
+    private String author;
+
+    private String publishingDate;
+
+    public News(String title, String sectionName) {
+        this.title = title;
+        this.sectionName = sectionName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSectionName() {
+        return sectionName;
+    }
+
+    public void setSectionName(String sectionName) {
+        this.sectionName = sectionName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublishingDate() {
+        return publishingDate;
+    }
+
+    public void setPublishingDate(String publishingDate) {
+        this.publishingDate = publishingDate;
+    }
+}
